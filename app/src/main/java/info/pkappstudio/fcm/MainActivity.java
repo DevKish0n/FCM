bf79a07376b5b2c89b4ed943e944eb6bfeedb655
Kishon Kumar Pashi <pritu111225@gmail.com>
2020-11-26 23:03:39 +0600

    First commit

---
+++ app/src/main/java/info/pkappstudio/fcm/MainActivity.java
@@ -0,0 +1,27 @@
+package info.pkappstudio.fcm;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()){
+                        Log.e( "FCM TOKEN: ", task.getResult());
+                    }
+                });
+    }
+}
